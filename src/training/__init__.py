e191d96898c63c3161001a8df09e53e5d6bab7d1
Vika <vikalinet@mail.ru>
2026-05-31 22:54:58 +0300

    fix lint tests

--- src/training/__init__.py
+++ src/training/__init__.py
@@ -6,8 +6,13 @@ from src.training.hyperparameter_tuning import HyperparameterTuner
 from src.training.model_comparison import ModelComparator
 from src.training.mlflow_integration import MLflowIntegration
 from src.training.improved_training import ImprovedModelTrainer
-from src.training.automl_training import AutoGluonTrainer
 from src.features.engineering import FeatureEngineer
+
+# AutoGluonTrainer импортируется условно, так как autogluon - опциональная зависимость
+try:
+    from src.training.automl_training import AutoGluonTrainer
+except ImportError:
+    AutoGluonTrainer = None  # type: ignore
 
 __all__ = [
     "BaseTrainer",
